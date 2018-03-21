a92ebaa63e075d456a8bae69b0d68c5a03979e4d
gabor horvath <hgabor47@gmail.com>
2018-03-21 12:13:51 +0100

    Branches solution

--- makebranch.bat
+++ makebranch.bat
@@ -1,12 +1,13 @@
 git add .
-git commit -m "%1"
+git commit -m %1
 git push origin master
-rd "..\temp"
+rd "..\temp" 
 md "..\temp"
 xcopy "main\c#" "..\temp" /E /C /Y
 git checkout c#
 xcopy "..\temp" "main\c#" /E /C /Y
 git add .
+git commit -m %1
 git push origin c#
 git checkout master
 
