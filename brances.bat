85a17551fa252720f5ef71c02468969b8ab1ae18
gabor horvath <hgabor47@gmail.com>
2018-03-21 09:30:36 +0100

    c# branch

---
+++ brances.bat
@@ -0,0 +1,10 @@
+git branch -d c#
+copy .gitignore_c# .gitignore
+git checkout -b c#
+git add .
+git commit -m "c# branch"
+git push origin c# 
+copy .gitignore_all .gitignore
+git checkout master
+
+
