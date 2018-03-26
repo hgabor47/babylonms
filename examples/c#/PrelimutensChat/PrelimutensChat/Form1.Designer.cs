6cd07491885a21135501cf9e33dabde0fa6d5c27
gabor horvath <hgabor47@gmail.com>
2018-03-26 16:15:49 +0200

    Test C# project created

---
+++ examples/c#/PrelimutensChat/PrelimutensChat/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace PrelimutensChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.me = new System.Windows.Forms.TextBox();
+            this.you = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ip = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // me
+            // 
+            this.me.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.me.Location = new System.Drawing.Point(12, 24);
+            this.me.Multiline = true;
+            this.me.Name = "me";
+            this.me.Size = new System.Drawing.Size(550, 137);
+            this.me.TabIndex = 0;
+            // 
+            // you
+            // 
+            this.you.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.you.Location = new System.Drawing.Point(12, 189);
+            this.you.Multiline = true;
+            this.you.Name = "you";
+            this.you.Size = new System.Drawing.Size(549, 156);
+            this.you.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Me";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "You                    IP:";
+            // 
+            // ip
+            // 
+            this.ip.Location = new System.Drawing.Point(118, 167);
+            this.ip.Name = "ip";
+            this.ip.Size = new System.Drawing.Size(161, 20);
+            this.ip.TabIndex = 4;
+            this.ip.Text = "192.168.1.120";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 354);
+            this.Controls.Add(this.ip);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.you);
+            this.Controls.Add(this.me);
+            this.Name = "Form1";
+            this.Text = "Prelimutens Chat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox me;
+        private System.Windows.Forms.TextBox you;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ip;
+    }
+}
+
